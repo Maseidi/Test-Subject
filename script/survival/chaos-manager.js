ee1e89dff1ffd59af254e3a49e0960088617f779
Maseidi <joanxerinho@gmail.com>
2025-03-30 16:32:52 +0330

    make health potion apper later

--- script/survival/chaos-manager.js
+++ script/survival/chaos-manager.js
@@ -251,12 +251,12 @@ const updateShop = () => {
 
     if (chaos === 20) vendingMachine.push(new ArmorShopItem())
 
-    if (chaos % 10 === 6) vendingMachine.push(new HealthPotionShopItem())
+    if (chaos % 10 === 0) vendingMachine.push(new HealthPotionShopItem())
 
     if (getChaos() > 10) {
-        if (chaos % 10 === 2 && getAdrenalinesDropped() < 10) vendingMachine.push(new AdrenalineShopItem())
+        if (chaos % 10 === 4 && getAdrenalinesDropped() < 10) vendingMachine.push(new AdrenalineShopItem())
 
-        if (chaos % 10 === 4 && getEnergyDrinksDropped() < 10) vendingMachine.push(new EnergyDrinkShopItem())
+        if (chaos % 10 === 6 && getEnergyDrinksDropped() < 10) vendingMachine.push(new EnergyDrinkShopItem())
 
         if (chaos % 10 === 8 && getLuckPillsDropped() < 10) vendingMachine.push(new LuckPillsShopItem())
     }
